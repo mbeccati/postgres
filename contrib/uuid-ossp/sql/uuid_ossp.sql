ee021036d681feda62db3c238207d73237c75fbc
Matteo Beccati <matteo@beccati.com>
2014-05-27 09:10:28 +0200

    Added v1mc test

--- contrib/uuid-ossp/sql/uuid_ossp.sql
+++ contrib/uuid-ossp/sql/uuid_ossp.sql
@@ -17,3 +17,20 @@ SELECT uuid_generate_v3(uuid_ns_dns(), 'www.widgets.com');
 SELECT uuid_generate_v5(uuid_ns_dns(), 'www.widgets.com');
 
 SELECT uuid_generate_v4()::text ~ '^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$';
+
+DO $_$
+DECLARE
+  u text;
+  i int;
+  c int;
+BEGIN
+  FOR i in 1..32 LOOP
+    u := substr(uuid_generate_v1mc()::text, 25, 2);
+    EXECUTE 'SELECT x''' || u || '''::int & 3' INTO c;
+    IF c <> 3 THEN
+      RAISE WARNING 'v1mc broken';
+    END IF;
+  END LOOP;
+END;
+$_$;
+
